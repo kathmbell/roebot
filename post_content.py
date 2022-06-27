06fc1655da2aa309f8583119ec8e7b4468983575
kathmbell <katherine.marie.bell@gmail.com>
2022-06-27 12:01:41 -0700

    post content

---
+++ post_content.py
@@ -0,0 +1,17 @@
+post_title = 'Safe & effective women\'s health resources' 
+
+# this is basically a direct rip off of #https://www.wired.com/story/guide-abortion-resources-post-roe-america/
+
+post_description = '''What abortion method is right for me?
+
+	Your abortion plan is going to look different depending on whether you opt for a surgical or medication abortion, so it’s good to learn about both and decide early on which option feels like the best fit for you. If you can do a medication abortion, it might be the better choice; as abortion clinics get swamped with clients, wait times for appointments increase. The longer you wait to get an abortion, the more complicated and expensive it can get. Ultimately, a medication abortion now can be a better option than a surgical abortion in a few weeks—even if a surgical abortion would be your first choice in an ideal scenario.
+
+	Surgical Abortion Search Resrouces
+
+	If you do choose a surgical abortion, your first stop should be Abortion Finder (https://www.abortionfinder.org/), a database of hundreds of trusted and verified abortion providers in the United States. Unlike Google, which sometimes turns up anti-abortion organizations when you search for an abortion clinic, Abortion Finder will show you only actual abortion providers, along with the relevant abortion laws in that state. If you live in a state with severe abortion restrictions, Abortion Finder’s filters allow you to find the nearest clinic that meets your needs—a clinic that doesn’t require a waiting period, one that will provide an abortion after six weeks, or whatever else you may be looking for.
+
+
+	Pill Ressources
+
+	If a medication abortion is a good option for you, there are even more resources available. While you can go to a brick-and-mortar clinic to get your abortion pills, research has repeatedly shown that taking these pills at home, by yourself, is just as safe as taking them in a doctor’s presence. (https://pubmed.ncbi.nlm.nih.gov/31859163/)(https://www.sciencedirect.com/science/article/abs/pii/S0010782419301763?via%3Dihub)(https://apps.who.int/iris/rest/bitstreams/1280116/retrieve)
+'''
