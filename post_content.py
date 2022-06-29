05e8ae4b656b6ec20a732c383311973a3cae3122
kathmbell <katherine.marie.bell@gmail.com>
2022-06-28 17:23:36 -0700

    updating html

--- post_content.py
+++ post_content.py
@@ -1,44 +1,7 @@
-post_title = 'Safe & effective women\'s health resources' 
+post_title = 'Safe & effective womens\' health resources' 
 
 # this is basically a direct rip off of #https://www.wired.com/story/guide-abortion-resources-post-roe-america/
 
-post_description = '''What abortion method is right for me?
-
-	Your abortion plan is going to look different depending on whether you opt for a surgical or medication abortion, so it’s good to learn about both and decide early on which option feels like the best fit for you. If you can do a medication abortion, it might be the better choice; as abortion clinics get swamped with clients, wait times for appointments increase. The longer you wait to get an abortion, the more complicated and expensive it can get. Ultimately, a medication abortion now can be a better option than a surgical abortion in a few weeks—even if a surgical abortion would be your first choice in an ideal scenario.
-
-	Surgical Abortion Search Resources
-
-	If you do choose a surgical abortion, your first stop should be Abortion Finder (https://www.abortionfinder.org/), a database of hundreds of trusted and verified abortion providers in the United States. Unlike Google, which sometimes turns up anti-abortion organizations when you search for an abortion clinic, Abortion Finder will show you only actual abortion providers, along with the relevant abortion laws in that state. If you live in a state with severe abortion restrictions, Abortion Finder’s filters allow you to find the nearest clinic that meets your needs—a clinic that doesn’t require a waiting period, one that will provide an abortion after six weeks, or whatever else you may be looking for.
-
-
-	Pill Resources
-
-	If a medication abortion is a good option for you, there are even more resources available. While you can go to a brick-and-mortar clinic to get your abortion pills, research has repeatedly shown that taking these pills at home, by yourself, is just as safe as taking them in a doctor’s presence. (https://pubmed.ncbi.nlm.nih.gov/31859163/)(https://www.sciencedirect.com/science/article/abs/pii/S0010782419301763?via%3Dihub)(https://apps.who.int/iris/rest/bitstreams/1280116/retrieve)
-
-	Plan C maintains a large database of verified abortion pill providers, including both telehealth services that provide support through the abortion process and online pharmacies that will mail you the pills with no additional support. (http://plancpills.org/)
-
-	Even if abortion is illegal where you are, abortion with pills is still an option. If you’re able to travel to a state where abortion is legal, a telehealth service can mail you the pills there. And if you’re not able to travel, the European site AidAccess (https://aidaccess.org/) will connect you with an Indian pharmacy that sends abortion pills to all 50 US states, regardless of abortion laws. 
-
-	Because abortion is time sensitive, and the process of getting pills sent from India can take a few weeks, AidAccess is also willing to prescribe abortion pills to people who aren’t currently pregnant in case they need them in the future. That’s right: You can go to AidAccess and get abortion pills right now. (How’s that for planning ahead?)
-
-	Other Resources on how to use the pill
-
-	How to use Abortion Pill (https://www.howtouseabortionpill.org/)
-	Self-managed Abortion: Safe and Supported (https://abortionpillinfo.org/)
-
-Emotional Support Resources 
-
-	Peer-based emotional support (https://reprocare.com/)
-
-Legal Support Resources
-
-	Repro Legal Helpline (https://www.ifwhenhow.org/repro-legal-helpline/) onfidential helpline that provides information and advice to people seeking to self-manage their abortions.
-	Repro Legal Defense Fund (http://reprolegaldefensefund.org/) the Repro Legal Defense Fund provides bail and legal defense for people who’ve been investigated, arrested, or prosecuted for self-managed abortion if you wind up getting in trouble,.
-
-Financial Resources
-	National Network of Abortion Funds (https://abortionfunds.org/need-abortion/) o an organization that will help you sort out your financial and logistical needs and do everything they can to remove the barriers between you and your abortion. 
-
-Donating
-	Help support donation access: https://donate.abortionfunds.org/give/323375/#!/donation/checkout
+post_description = '''<h2>What abortion method is right for me?</h2></br><p>Your abortion plan is going to look different depending on whether you opt for a surgical or medication abortion, so it’s good to learn about both and decide early on which option feels like the best fit for you. If you can do a medication abortion, it might be the better choice; as abortion clinics get swamped with clients, wait times for appointments increase. The longer you wait to get an abortion, the more complicated and expensive it can get. Ultimately, a medication abortion now can be a better option than a surgical abortion in a few weeks—even if a surgical abortion would be your first choice in an ideal scenario.</p></br><h2>Surgical Abortion Search Resources</h2></br><p>If you do choose a surgical abortion, your first stop should be <a href = "https://www.abortionfinder.org/">Abortion Finder</a>, a database of hundreds of trusted and verified abortion providers in the United States. Unlike Google, which sometimes turns up anti-abortion organizations when you search for an abortion clinic, Abortion Finder will show you only actual abortion providers, along with the relevant abortion laws in that state. If you live in a state with severe abortion restrictions, Abortion Finder’s filters allow you to find the nearest clinic that meets your needs—a clinic that doesn’t require a waiting period, one that will provide an abortion after six weeks, or whatever else you may be looking for.</p></br><h2>Pill Resources</h2></br><p>If a medication abortion is a good option for you, there are even more resources available. While you can go to a brick-and-mortar clinic to get your abortion pills, research has repeatedly shown that taking these pills at home, by yourself, <a href="https://pubmed.ncbi.nlm.nih.gov/31859163/">is just as safe</a> <a href="https://www.sciencedirect.com/science/article/abs/pii/S0010782419301763?via%3Dihub">as taking them</a> <a href="https://apps.who.int/iris/rest/bitstreams/1280116/retrieve">in a doctor’s presence</a>.</p></br><p><a href="http://plancpills.org/">Plan C</a> maintains a large database of verified abortion pill providers, including both telehealth services that provide support through the abortion process and online pharmacies that will mail you the pills with no additional support.</p></br><p>Even if abortion is illegal where you are, abortion with pills is still an option. If you’re able to travel to a state where abortion is legal, a telehealth service can mail you the pills there. And if you’re not able to travel, the European site <a href = "https://aidaccess.org/">AidAccess</a> will connect you with an Indian pharmacy that sends abortion pills to all 50 US states, regardless of abortion laws.</p></br><p>Because abortion is time sensitive, and the process of getting pills sent from India can take a few weeks, AidAccess is also willing to prescribe abortion pills to people who aren’t currently pregnant in case they need them in the future. That’s right: You can go to AidAccess and get abortion pills right now. (How’s that for planning ahead?)</p></br><h2>Other Resources on how to use the pill</h2></br><ul><a href = "https://www.howtouseabortionpill.org/">How to use Abortion Pill</a></br><a href = "https://abortionpillinfo.org/" Self-managed Abortion: Safe and Supported</a></ul></br><h2>Emotional Support Resources</h2></br><ul><a href = "https://reprocare.com/">Peer-based emotional support</a></ol></br>Legal Support Resources</h2></br><p><a href = "https://www.ifwhenhow.org/repro-legal-helpline/">Repro Legal Helpline</a> is a confidential helpline that provides information and advice to people seeking to self-manage their abortions.</p></br><p><a href = "http://reprolegaldefensefund.org/">Repro Legal Defense Fund</a> provides bail and legal defense for people who’ve been investigated, arrested, or prosecuted for self-managed abortion if you wind up getting in trouble.</p><h2>Financial Resources</h2></br><p><a href = "https://abortionfunds.org/need-abortion/">National Network of Abortion Funds</a> is an organization that will help you sort out your financial and logistical needs and do everything they can to remove the barriers between you and your abortion. </p></br><h2>Donating</h2></br><p><a href = "https://donate.abortionfunds.org/give/323375/#!/donation/checkout" Help support donation access</a></p></br>
 
 '''
